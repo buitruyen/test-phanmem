f003b430e493106e4827c9c8f87d3608ac549e14
buitruyen <namtruyenpro95@gmail.com>
2018-12-08 21:03:11 +0700

    ok

--- module/Started/config/module.config.php
+++ module/Started/config/module.config.php
@@ -1,50 +1,62 @@
 <?php
-/**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
+	/**
+	 * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+	 * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+	 * @license   http://framework.zend.com/license/new-bsd New BSD License
+	 */
+	
+	namespace Started;
+	
+	use Zend\Router\Http\Literal;
+	use Zend\Router\Http\Segment;
+	use Zend\ServiceManager\Factory\InvokableFactory;
+	
+	return [
+		'router'       => [
+			'routes' => [
+//            'started' => [ //Name router
+//                'type'    => Segment::class, //type router
+//                'options' => [
+//                    'route'    => '/started[/:action]',
+//                    'defaults' => [
+//                        'controller' => Controller\IndexController::class,
+//                        'action'     => 'index',
+//                    ],
+//                ],
+//            ],
 
-namespace Started;
+'started' => [
+	'type'    => Literal::class ,
+	'options' => [
+		'route'    => '/started' ,
+		'defaults' => [
+			'controller' => Controller\IndexController::class ,
+			'action'     => 'index' ,
+		] ,
+	] ,
+] ,
 
-use Zend\Router\Http\Literal;
-use Zend\Router\Http\Segment;
-use Zend\ServiceManager\Factory\InvokableFactory;
-
-return [
-    'router' => [
-        'routes' => [
-            'started' => [
-                'type'    => Segment::class,
-                'options' => [
-                    'route'    => '/started[/:action]',
-                    'defaults' => [
-                        'controller' => Controller\IndexController::class,
-                        'action'     => 'index',
-                    ],
-                ],
-            ],
-            'training' => [
-	            'type'    => Segment::class,
-	            'options' => [
-		            'route'    => '/training/index/[/:action]',
-		            'defaults' => [
-			            'controller' => Controller\TestController::class,
-			            'action'     => 'index',
-		            ],
-	            ],
-            ],
-        ],
-    ],
-    'controllers' => [
-        'factories' => [
-            Controller\IndexController::class => InvokableFactory::class,
-        ],
-    ],
-    'view_manager' => [
-       
-        'template_path_stack' => [
-            __DIR__ . '/../view',
-        ],
-    ],
-];
+'started-edit' => [
+	'type'    => Literal::class ,
+	'options' => [
+		'route'    => '/started/edit' ,
+		'defaults' => [
+			'controller' => Controller\IndexController::class ,
+			'action'     => 'edit' ,
+		] ,
+	] ,
+] ,
+			] ,
+		] ,
+		'controllers'  => [
+			'factories' => [
+				Controller\IndexController::class => InvokableFactory::class ,
+			] ,
+		] ,
+		'view_manager' => [
+			
+			'template_path_stack' => [
+				__DIR__.'/../view' ,
+			] ,
+		] ,
+	];
