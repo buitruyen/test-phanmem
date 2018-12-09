2bd249003baca46c4c6c439690769b688b1ed9b0
buitruyen <namtruyenpro95@gmail.com>
2018-12-09 12:08:05 +0700

    p2

---
+++ module/Started/config/ini/controller-view.php
@@ -0,0 +1,26 @@
+<?php
+	/**
+	 * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+	 * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+	 * @license   http://framework.zend.com/license/new-bsd New BSD License
+	 */
+	
+	namespace Training;
+	
+	use Zend\Router\Http\Literal;
+	use Zend\Router\Http\Segment;
+	use Zend\ServiceManager\Factory\InvokableFactory;
+	
+	return [
+		'controllers'  => [
+			'factories' => [
+				Controller\IndexController::class  => InvokableFactory::class ,
+				Controller\ConfigController::class => InvokableFactory::class ,
+			] ,
+		] ,
+		'view_manager' => [
+			'template_path_stack' => [
+				__DIR__.'/../../view' ,
+			] ,
+		] ,
+	];
