c3fdc457e3bad65efc2760cda1e060333ef0f4cc
buitruyen <namtruyenpro95@gmail.com>
2018-12-10 23:45:07 +0700

    ok

--- module/Started/src/Controller/LearningZendFramWork.php
+++ module/Started/src/Controller/LearningZendFramWork.php
@@ -12,61 +12,74 @@
 	
 	class LearningZendFramWork extends AbstractActionController{
 		
-		public function index01Action(){
+		public function index04Action(){
 			echo '<h3 style="color: #0000CC;font-weight: bold">'.__METHOD__.'</h3>';
 			
-			$loader = new StandardAutoloader([ 'autoregister_zf' => TRUE ]);
-			$loader -> registerNamespace('Database' , LIBARY_PATH.'/Database');
-			$loader -> registerPrefix('Mail' , LIBARY_PATH.'/Mail');
+			$loader = new ClassMapAutoloader();
+			// Register the class map:
+			$loader -> registerAutoloadMap(LIBARY_PATH.'/Autoload/autoload_classmap.php');
 			$loader -> register();
+			
 			$student = new Student();
 			$teacher = new Teacher();
 			$worker  = new Worker();
-			
-			$sender  = new \Mail_Sender();
-			$attache = new \Mail_Attache();
 			$sender2 = new \Mail_Abc_Sender();
-			
 			return FALSE;
+			
 		}
 		
-		public function index02Action(){
+		public function index05Action(){
 			echo '<h3 style="color: #0000CC;font-weight: bold">'.__METHOD__.'</h3>';
 			
-			$loader = new StandardAutoloader(
+			$loader = new ClassMapAutoloader(
 				[
-					'autoregister_zf' => TRUE ,
-					'namespaces'      => [
-						'Database' => LIBARY_PATH.'/Database' ,
-						'File\Abc' => LIBARY_PATH.'/File' ,
-					] ,
-					
-					'prefixes' => [
-						'Mail' => LIBARY_PATH.'/Mail' ,
-					] ,
-				
+					LIBARY_PATH.'/Autoload/autoload_classmap.php' ,
+					LIBARY_PATH.'/Autoload/classmap.php',
 				]
-			
 			);
 			
 			$loader -> register();
+			
 			$student = new Student();
 			$teacher = new Teacher();
 			$worker  = new Worker();
-			
-			$sender  = new \Mail_Sender();
-			$attache = new \Mail_Attache();
 			$sender2 = new \Mail_Abc_Sender();
 			$upload  = new \File\Abc\Upload();
-			
 			return FALSE;
+			
 		}
 		
-		public function index03Action(){
+		public function index06Action(){
 			echo '<h3 style="color: #0000CC;font-weight: bold">'.__METHOD__.'</h3>';
 			
 			AutoloaderFactory ::factory(
 				[
+					'Zend\Loader\ClassMapAutoloader' => [
+						LIBARY_PATH.'/Autoload/autoload_classmap.php' ,
+						LIBARY_PATH.'/Autoload/classmap.php',
+					] ,
+				]
+			
+			);
+			
+			$student = new Student();
+			$teacher = new Teacher();
+			$worker  = new Worker();
+			$sender2 = new \Mail_Abc_Sender();
+			$upload  = new \File\Abc\Upload();
+			return FALSE;
+			
+		}
+		
+		public function index07Action(){
+			echo '<h3 style="color: #0000CC;font-weight: bold">'.__METHOD__.'</h3>';
+			
+			AutoloaderFactory ::factory(
+				[
+					'Zend\Loader\ClassMapAutoloader' => [
+						LIBARY_PATH.'/Autoload/autoload_classmap.php' ,
+						LIBARY_PATH.'/Autoload/classmap.php',
+					] ,
 					'Zend\Loader\StandardAutoloader' => [
 						'autoregister_zf' => TRUE ,
 						'namespaces'      => [
@@ -81,16 +94,14 @@
 				]
 			
 			);
+			
 			$student = new Student();
 			$teacher = new Teacher();
 			$worker  = new Worker();
-			
-			$sender  = new \Mail_Sender();
-			$attache = new \Mail_Attache();
 			$sender2 = new \Mail_Abc_Sender();
 			$upload  = new \File\Abc\Upload();
-			
 			return FALSE;
+			
 		}
 		
 	
